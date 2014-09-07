443277beb91ba9f986194becb5a2174d29c8f3cb
Cristian R. Arroyo <cristian.arroyo@vivaserver.com>
2014-09-06 21:57:54 -0300

    concept for CadaDeCambioBTC.com support

--- javascripts/app.js
+++ javascripts/app.js
@@ -154,6 +154,43 @@ var app = function() {
     return exchanger;
   };
 
+  var CasaDeCambio = function() {
+    var exchanger = {};
+
+    // needed to cache/parse exchanger quotes
+    var conf = {
+      quote: function(data, use_data_time) {
+        var data_time = new Date();
+        if ((data || {}).bid && (data || {}).ask) {
+          if (use_data_time) {  // force expired date
+            data_time = new Date(data_time.getFullYear(),data_time.getMonth(),data_time.getDate()-1);
+          }
+          return {
+            buy: {
+              ars:  data.ask,
+              time: data_time
+            },
+            sell: {
+              ars:  data.bid,
+              time: data_time
+            },
+            // blue: usd_ars.sell/usd_ars.buy
+            created_at: data_time  // "2014-07-09T17:13:34.553Z"
+          };
+        }
+        else {
+          return false;
+        }
+      },
+      cache: "/javascripts/cache.casadecambio.json",
+      name: "casadecambio",
+      URI: "https://www.casadecambiobtc.com/api/quotations/BTCARS"
+    };
+    exchanger = exchangeable(conf);
+
+    return exchanger;
+  };
+
   var Home = function() {
     var $buy, $sell, $time;
 
@@ -272,7 +309,7 @@ var app = function() {
       moment.lang("es");
 
       $el = $elem;
-      exchange = DigiCoins();  // TODO: setup on-demand
+      exchange = CasaDeCambio();  // TODO: setup on-demand
 
       Home.init($el);
       // render from local cache JSON file
