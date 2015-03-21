2c0b9032892bacdbbbbcf45a6b3319219d3cde06
Cristian R. Arroyo <cristian.arroyo@vivaserver.com>
2015-03-21 18:39:33 -0300

    broken require1k concept for DigiCoins

--- javascripts/app.js
+++ javascripts/app.js
@@ -5,90 +5,6 @@
 
 var $el, exchange, cache_timeout = 10;  // in minutes
 
-var exchangeable = function(exchange) {
-  var backendURL = function() {
-    return (window.location.port ? window.location.href : "http://btc-ars.herokuapp.com/") + "quote/";
-  };
-
-  var updateCache = function(data, use_data_time) {
-    console.log(data);
-    localforage.getItem(exchange.name,function(cache) {
-      var quote, current, previous = {"previous":null};
-      if (!!cache) {
-        previous = {"previous":cache.current};
-      }
-      quote = exchange.quote(data,use_data_time);
-      if (!!quote) {
-        current = $.extend({"current":quote},previous);
-        localforage.setItem(exchange.name,current,function() {
-          $el.trigger("data:change");
-        });
-      }
-      else {
-        $el.trigger("data:error");
-      }
-    });
-  };
-
-  var updateFrom = function(uri, use_data_time) {
-    // ask quotes via backend to avoid CORS; but mind cache files are stored locally
-    var targetURI = uri.substring(0,4)==="http" ? backendURL()+uri : uri; 
-    $.ajax({
-      dataType: "json",
-      type: "GET",
-      url: targetURI, 
-      success: function(data) {
-        updateCache(data,use_data_time);
-      },
-      error: function(xhr, type) {
-        console.log(type);  // "abort"
-        $el.trigger("data:error");
-      }
-    });
-  };
-
-  var lapseExpired = function(cache) {
-    var then  = moment(cache.created_at), now = moment();  // mind created_at
-    var diff  = moment(now).diff(moment(then));
-    var lapse = moment.duration(diff).asMinutes();
-    return lapse;
-  };
-
-  return {
-    current: function(callBack) {
-      localforage.getItem(exchange.name,function(cache) {
-        callBack(cache && cache.current ? cache.current : null);
-      });
-    },
-    previous: function(callBack) {
-      localforage.getItem(exchange.name,function(cache) {
-        callBack(cache && cache.previous ? cache.previous : null);
-      });
-    },
-    blue: function(quote) {
-      if (!!quote && quote.ars && quote.usd) {
-        return quote.ars/quote.usd;
-      }
-    },
-    name: function() {
-      return exchange.name;
-    },
-    update: function() {
-      localforage.getItem(exchange.name,function(cache) {
-        if (!cache || lapseExpired(cache.current) > cache_timeout-1) {
-          updateFrom(exchange.URI);
-        }
-      });
-    },
-    updateFromLocal: function() {
-      var use_data_time = true;
-      localforage.removeItem(exchange.name,function() {
-        updateFrom(exchange.cache,use_data_time);
-      });
-    }
-  };
-};
-
 var app = function() {
   var Home = function() {
     var $buy, $sell, $time;
@@ -249,7 +165,7 @@ var app = function() {
                 exchange = ConectaBitcoin();
               break;
               case "digicoins":
-                exchange = DigiCoins();
+                exchange = require("./exchange/digicoins");
               break;
               case "ripio":
                 exchange = Ripio();
@@ -269,7 +185,7 @@ var app = function() {
       moment.lang("es");
 
       $el = $(elem || "body");
-      exchange = CasaDeCambio();  // TODO: get from localforage
+      exchange = require("./exchange/digicoins");  // TODO: get from localforage
 
       Home.init();
       // render from local cache JSON file
